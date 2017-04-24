6089b7dbffbb4a1ccc828a98d82c35348478ba64
zkakol <zkako001@odu.edu>
2017-04-23 23:47:43 -0400

    Fix issue in app

--- polls/pinax-blog-master/pinax/blog/__init__.py
+++
@@ -1,5 +0,0 @@
-import pkg_resources
-
-
-__version__ = pkg_resources.get_distribution("pinax-blog").version
-default_app_config = "pinax.blog.apps.AppConfig"
--- polls/pinax-blog-master/pinax/blog/context_processors.py
+++
@@ -1,8 +0,0 @@
-from .conf import settings
-
-
-def scoped(request):
-    return {
-        "scoper_lookup": request.resolver_match.kwargs.get(settings.PINAX_BLOG_SCOPING_URL_VAR)
-        if request.resolver_match else ""
-    }
